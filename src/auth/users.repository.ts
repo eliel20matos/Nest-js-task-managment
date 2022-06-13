9e41b310d2597f8b7cf9a066cf0d335d4adea68a
eliel20matos <eliellimamatos@gmail.com>
2022-06-13 09:32:13 -0400

    feat: Implementing bcrypt to hash and store an password

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -2,17 +2,21 @@ import {
   ConflictException,
   InternalServerErrorException,
 } from '@nestjs/common';
-import { report } from 'process';
 import { CustomRepository } from 'src/database/typeorm-ex.decorator';
 import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @CustomRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
 
     try {
       await this.save(user);
