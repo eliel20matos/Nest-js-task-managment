ef8d5c347caf374c1e1825d2dfb288477ce430e6
eliel20matos <eliellimamatos@gmail.com>
2022-06-13 08:33:27 -0400

    feat: creating an user

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,8 +1,19 @@
-import { CustomRepository } from "src/database/typeorm-ex.decorator";
-import { Repository } from "typeorm";
-import { User } from "./user.entity";
+import { report } from 'process';
+import { CustomRepository } from 'src/database/typeorm-ex.decorator';
+import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
 
 @CustomRepository(User)
 export class UsersRepository extends Repository<User> {
-    //
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.create({
+      username,
+      password,
+    });
+
+    await this.save(user);
+  }
 }
