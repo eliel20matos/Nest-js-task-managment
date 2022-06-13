1fd32b29e8b3d85c2c5d658146b26fa98c6c1e47
eliel20matos <eliellimamatos@gmail.com>
2022-06-13 10:07:38 -0400

    feat: Adding jwt modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,20 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersRepository } from './users.repository';
 import { TypeOrmExModule } from 'src/database/typeorm-ex.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmExModule.forCustomRepository([UsersRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600,
+      }
+    }),
+    TypeOrmExModule.forCustomRepository([UsersRepository],)
+  ],
   providers: [AuthService],
   controllers: [AuthController],
   exports: [AuthService],
